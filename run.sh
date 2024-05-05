a20afa0c69ec24dd91ef8f88df408c62234f0d7c
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-05 17:47:49 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+export SOURCE_DB=postgresql://$DB_USER:$DB_PASSWORD@$DB_ENDPOINT/$DB_NAME
 psql $SOURCE_DB -f scripts/oltp_init.sql
 python3 download.py
 python3 populate.py
